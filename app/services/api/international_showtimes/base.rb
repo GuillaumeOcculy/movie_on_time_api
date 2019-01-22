b38c70ab775997467de8236ff649422cb5a899fd
Guillaume <occuly.guillaume@gmail.com>
2019-01-22 12:04:31 +0100

    Add Api::InternationalShowtimes service api
    Add config & httparty gems
    We can fetch all informations on InternationalShowtimes

---
+++ app/services/api/international_showtimes/base.rb
@@ -0,0 +1,51 @@
+module Api::InternationalShowtimes
+  class Base
+    include HTTParty
+    
+    base_uri Settings.international_showtimes.base_url
+
+    def initialize
+      # @country = Country.find_by(name: 'France')
+      @options = {headers: {'Content-Type' => 'application/json', 'Accept' => 'application/json', 'X-API-Key' => Settings.international_showtimes.api_key}, format: :plain}
+    end
+
+    def genres
+      JSON.parse(self.class.get('/genres', @options), symbolize_names: true)[:genres]
+    end
+
+    def cities
+      JSON.parse(self.class.get('/cities', @options), symbolize_names: true)[:cities]
+    end
+
+    def chains
+      JSON.parse(self.class.get('/chains', @options), symbolize_names: true)[:chains]
+    end
+
+    def cinema(id)
+      JSON.parse(self.class.get("/cinemas/#{id}", @options), symbolize_names: true)[:cinema]
+    end
+    
+    def cinemas
+      JSON.parse(self.class.get('/cinemas', @options), symbolize_names: true)[:cinemas]
+    end
+
+    def upcoming_movies(country: 'FR')
+      @options[:query] = { release_date_from: Date.today, countries: country, include_upcomings: true }
+      JSON.parse(self.class.get('/movies', @options), symbolize_names: true)[:movies].select{ |x| x[:id].present? }
+    end
+
+    def movie_details(id, lang)
+      @options[:headers]['Accept-Language'] = lang
+      JSON.parse(self.class.get("/movies/#{id}", @options), symbolize_names: true)[:movie]
+    end
+
+    def showtimes(cinema_id, options = {})
+      @options[:query] = { cinema_id: cinema_id }
+      JSON.parse(self.class.get('/showtimes', @options), symbolize_names: true)[:showtimes].select{|showtime| showtime[:cinema_id].present? && showtime[:movie_id].present? }
+    end
+
+    def showtime_details(id)
+      JSON.parse(self.class.get("/showtimes/#{id}", @options), symbolize_names: true)
+    end
+  end
+end

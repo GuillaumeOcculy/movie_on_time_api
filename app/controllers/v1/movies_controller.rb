0e91ec4398a3a6625d85a55bf1eba8cc486ce1fe
Guillaume <occuly.guillaume@gmail.com>
2019-03-24 19:33:33 +0100

    Improve params on movies#show

--- app/controllers/v1/movies_controller.rb
+++ app/controllers/v1/movies_controller.rb
@@ -20,11 +20,20 @@ class V1::MoviesController < ApplicationController
   end
 
   def show
-    param! :date, Date, blank: true
+    param! :date, String, blank: true
     param! :query, String
 
     movie = Movie.find(params[:id])
-    date = params[:date] || movie.first_live_showtime&.start_date
-    render json: MovieSerializer.new(movie, params: {movie_id: movie.id, date: date, query: params[:query]}, include: [:directors, :casts, :trailers, :genres, :cinemas]).serialized_json
+    date = selected_date || movie.first_live_showtime&.start_date
+    render json: MovieSerializer.new(movie, params: {movie_id: movie.id, date: date, query: selected_query}, include: [:directors, :casts, :trailers, :genres, :cinemas]).serialized_json
+  end
+
+  private
+  def selected_date
+    params[:date] if params[:date].present? 
+  end
+
+  def selected_query
+    params[:query] if params[:query].present? 
   end
 end

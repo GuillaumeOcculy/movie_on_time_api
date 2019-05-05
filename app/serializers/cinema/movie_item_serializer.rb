284c2c627f0f84774907f7dc19ebc20bb9e6af1c
Guillaume <occuly.guillaume@gmail.com>
2019-05-05 22:45:49 +0200

    Update movie_item_serializer.rb

--- app/serializers/cinema/movie_item_serializer.rb
+++ app/serializers/cinema/movie_item_serializer.rb
@@ -2,6 +2,14 @@ class Cinema::MovieItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :poster_url, :running_time, :thumbnail_url, :release_date
 
+  attribute :watchlisted do |object, params|
+    object.watchlisted?(params[:current_user]) if params[:current_user]
+  end
+
+  attribute :watched do |object, params|
+    object.watched?(params[:current_user]) if params[:current_user]
+  end
+
   attribute :casts do |object|
     CastSerializer.new(object.casts).serializable_hash
   end

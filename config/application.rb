86341080e8e88d966c074c9caff53c16ad5dbb9d
Guillaume <occuly.guillaume@gmail.com>
2019-05-30 21:43:13 +0200

    Add debug to CORS

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module MovieOnTimeApi
       request_specs: false
     end
 
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
 

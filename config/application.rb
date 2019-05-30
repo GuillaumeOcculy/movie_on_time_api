2afb27e4d7aebe2e62ba849542a59cedf35240a0
Guillaume <occuly.guillaume@gmail.com>
2019-05-30 21:16:24 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,11 @@ module MovieOnTimeApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*',
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        origins '*'
+
+        resource '*', 
+        headers: :any, 
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
   end

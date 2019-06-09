d9a156835c1a64fa7f599c75f2aebca2adb644ac
Guillaume <occuly.guillaume@gmail.com>
2019-06-09 20:40:26 +0200

    Update showtime_spec.rb

--- spec/models/showtime_spec.rb
+++ spec/models/showtime_spec.rb
@@ -12,4 +12,35 @@ RSpec.describe Showtime, type: :model do
 
   it { is_expected.to belong_to(:movie) }
   it { is_expected.to belong_to(:cinema) }
+
+  describe '#start_time' do
+    it 'returns the correct time' do
+      showtime = build(:showtime, start_at: DateTime.new(2019, 1, 1, 10, 19))
+      expect(showtime.start_time).to eq '10:19'
+    end
+  end
+
+  describe '#end_time' do
+    it 'returns the correct time' do
+      showtime = build(:showtime, end_at: DateTime.new(2019, 1, 1, 19, 10))
+      expect(showtime.end_time).to eq '19:10'
+    end
+
+    it 'returns the nil if end_at is absent' do
+      showtime = build(:showtime)
+      expect(showtime.end_time).to be_nil
+    end
+  end
+
+  describe '#version' do
+    it 'returns VF version' do
+      showtime = build(:showtime, language: 'fr')
+      expect(showtime.version).to eq 'VF'
+    end
+
+    it 'returns VO version' do
+      showtime = build(:showtime, language: 'en')
+      expect(showtime.version).to eq 'VO'
+    end
+  end
 end

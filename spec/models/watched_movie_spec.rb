3440b7652a29db2df448378a004b14d90518608e
Guillaume <occuly.guillaume@gmail.com>
2019-06-09 21:06:20 +0200

    Update watched_movie_spec.rb

--- spec/models/watched_movie_spec.rb
+++ spec/models/watched_movie_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe WatchedMovie, type: :model do
 
   it "has a valid factory" do
-    expect(FactoryBot.build(:watched_movie)).to be_valid 
+    expect(build(:watched_movie)).to be_valid 
   end
-
-  it { is_expected.to validate_presence_of(:watched_date) }
 
   it { is_expected.to belong_to(:movie) }
   it { is_expected.to belong_to(:user) }
+
+  it 'saves watched_date to today if not specified' do
+    watched_movie = create(:watched_movie, watched_date: nil)
+    expect(watched_movie.watched_date).to eq Date.today
+  end
 end

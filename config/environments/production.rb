bd80e0281a4eff54adef643e08c89047ea45617a
Guillaume <occuly.guillaume@gmail.com>
2019-06-10 23:27:17 +0200

    Set config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'www.movieontime.com' }
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,

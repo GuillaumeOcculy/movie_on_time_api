5663313f982756aa47688cb91744c964ca1b08bc
Guillaume <occuly.guillaume@gmail.com>
2019-06-10 23:34:33 +0200

    Remove enable_starttls_auto

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     domain:               Rails.application.credentials.email[:domain],
     user_name:            Rails.application.credentials.email[:username],
     password:             Rails.application.credentials.email[:password],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    authentication:       'plain'
+    # enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
